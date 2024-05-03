f07d06ce87dc59f9fac801755cdc6fd79ff56b14
GOATDevop <167458519+GOATDevop@users.noreply.github.com>
2024-05-03 16:44:57 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.getElementById('connectBtn');
+    button.textContent = 'Connect Wallet'; // Define o texto inicial do botão
 
     // Função para detectar se a carteira Phantom está instalada
     function isPhantomInstalled() {
@@ -15,12 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         try {
-            // Solicita a conexão à carteira Phantom com configuração para sempre mostrar o popup de permissão
             const response = await window.solana.connect({ onlyIfTrusted: false });
             console.log('Connected to Phantom Wallet:', response.publicKey.toString());
             button.textContent = 'Disconnect from Phantom';
         } catch (error) {
-            // Tratamento específico para a rejeição do usuário
             if (error.message.includes("User rejected the request")) {
                 alert("Connection request was rejected. Please allow the connection in your Phantom wallet.");
             } else {
@@ -32,19 +31,20 @@ document.addEventListener('DOMContentLoaded', function () {
     // Função para desconectar da carteira Phantom
     async function disconnectWallet() {
         await window.solana.disconnect();
-        button.textContent = 'Connect to Phantom';
         console.log('Disconnected from Phantom Wallet');
+        button.textContent = 'Connect Wallet';
     }
 
     // Listener para o botão
     button.addEventListener('click', async () => {
-        if (button.textContent === 'Connect to Phantom') {
+        if (button.textContent === 'Connect Wallet') {
             await connectWallet();
         } else {
             await disconnectWallet();
         }
     });
 });
+
 
 
 
