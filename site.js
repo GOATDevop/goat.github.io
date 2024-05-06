38bd87779b0574fe88d1cb81093117435379f30e
GOATDevop <167458519+GOATDevop@users.noreply.github.com>
2024-05-06 23:52:40 +0100

    Update site.js

--- site.js
+++ site.js
@@ -94,7 +94,8 @@ document.addEventListener('DOMContentLoaded', function () {
         updateCarousel();
     }, 5000); // Altera a imagem/vídeo a cada 5 segundos
 
-  const startTime = new Date("Dec 25, 2023 15:00:00").getTime(); // Set the start date here
+    const startTime = new Date("MAY 07, 2024 00:00:00").getTime(); // Set the start date here
+    const endTime = new Date("MAY 09, 2024 23:00:00").getTime(); // Data de término da presale
     const timerElement = document.getElementById('timer');
     const presaleStartsInElement = document.getElementById('presaleStartsIn');
     const liveElement = document.getElementById('presaleLive');
